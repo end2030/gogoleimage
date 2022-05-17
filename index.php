8e75ca4fbaf13ccec8b1cd0d5b6ab90d211e291f
Zanri Nova Chalimi <32398181+end2030@users.noreply.github.com>
2022-05-17 13:19:52 +0700

    Update index.php

--- index.php
+++ index.php
@@ -38,7 +38,7 @@ if (count($datas)>100) {
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <title>Career Tes</title>
+    <title>Zanri Nova C</title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
